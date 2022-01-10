1115a3decc0a799e68d6daf51607a97b349cba2b
Andre Burke <aburke.dev@gmail.com>
2022-01-09 21:57:04 -0700

    initial commit

---
+++ DotEnv.cs
@@ -0,0 +1,29 @@
+namespace ABU.Portfolio;
+
+public static class DotEnv
+{
+    public static void LoadEnvironmentVariables()
+    {
+        var root = Directory.GetCurrentDirectory();
+        var dotenv = Path.Combine(root, ".env");
+        
+        SetEnvironmentVariables(dotenv);
+    }
+    
+    private static void SetEnvironmentVariables(string filePath)
+    {
+        if (!File.Exists(filePath))
+            return;
+
+        const int envVariableLength = 2;
+        foreach (var line in File.ReadAllLines(filePath))
+        {
+            var parts = line.Split('=', StringSplitOptions.RemoveEmptyEntries);
+
+            if (parts.Length != envVariableLength)
+                continue;
+            
+            Environment.SetEnvironmentVariable(parts[0].Trim(), parts[1].Trim());
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,30 @@
+using ABU.Portfolio;
+
+var builder = WebApplication.CreateBuilder(args);
+var services = builder.Services;
+
+DotEnv.LoadEnvironmentVariables();
+builder.Configuration.AddEnvironmentVariables();
+// Add services to the container.
+
+services.AddControllers();
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+services.AddEndpointsApiExplorer();
+services.AddSwaggerGen();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
