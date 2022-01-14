72d6172b9cb944eafb46dd2c12448c53d694da4c
Andre Burke <aburke.dev@gmail.com>
2022-01-14 00:00:31 -0700

    added the repos get via client in the github background service

--- Workers/GitHubBackgroundService.cs
+++ Workers/GitHubBackgroundService.cs
@@ -13,30 +13,31 @@ public class GitHubBackgroundService : BackgroundService
         _provider = Guard.Against.Null(provider, nameof(provider));
         _logger = Guard.Against.Null(logger, nameof(logger));
     }
-    
+
     protected override async Task ExecuteAsync(CancellationToken stoppingToken)
     {
-        const string workerName = nameof(GitHubBackgroundService);
+        var methodInfo = System.Reflection.MethodBase.GetCurrentMethod();
+        var fullName = $"{methodInfo?.DeclaringType?.FullName} - {methodInfo?.Name}";
+
         while (!stoppingToken.IsCancellationRequested)
         {
             try
             {
-                _logger.Log(LogLevel.Information, message: $"{workerName} started at: {DateTime.Now}");
+                _logger.Log(LogLevel.Information, message: $"{fullName} started at: {DateTime.Now}");
                 var scope = _provider.CreateScope();
 
                 var client = scope.ServiceProvider.GetRequiredService<IGitHubApiClient>();
+
+                var repoResult = await client.GetRepositoriesForUserAsync(stoppingToken);
             }
             catch (Exception ex)
             {
-                var methodInfo = System.Reflection.MethodBase.GetCurrentMethod();
-                var fullName = $"{methodInfo?.DeclaringType?.FullName} - {methodInfo?.Name}";
-                
                 _logger.Log(LogLevel.Error, $"An unexpected error occurred at: {DateTime.Now}, in => {fullName}. Message: {ex.Message}", ex);
             }
 
-            await Task.Delay(TimeSpan.FromDays(2), stoppingToken);
+            await Task.Delay(TimeSpan.FromDays(10), stoppingToken);
         }
-        
-        _logger.Log(LogLevel.Information, $"{workerName} stopped at: {DateTime.Now}");
+
+        _logger.Log(LogLevel.Information, $"{fullName} stopped at: {DateTime.Now}");
     }
 }
